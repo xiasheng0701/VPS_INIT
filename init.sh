54cf44cf3b0d2e39f1028d005ad57f874fbd7c7f
xs71 <liuxs0701@gmail.com>
2021-09-20 22:25:02 +0800

    更新地址

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@
 #Version :   1.1
 
 [ ! -d ~/.ssh ] && mkdir ~/.ssh
-wget -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/xs0701/vps_init/master/authorized_keys
-wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/xs0701/vps_init/master/sshd_config
+wget -O ~/.ssh/authorized_keys https://raw.githubusercontent.com/xs71/vps_init/master/authorized_keys
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/xs71/vps_init/master/sshd_config
 # fix sftp for Centos
 if [ -f /etc/redhat-release ]; then
     sed -i '77cSubsystem sftp /usr/libexec/openssh/sftp-server' /etc/ssh/sshd_config
